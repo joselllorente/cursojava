94415a606e3d2fb959a9823b75ea6b64892ee8c8
Tardes <Tardes@GET-PROF>
2024-12-13 19:16:51 +0100

    Arrays Bidimensionales Ejemplos + Ejercicios

---
+++ src/es/cursojava/inicio/arrays/ArraysBidimensionales.java
@@ -0,0 +1,20 @@
+package es.cursojava.inicio.arrays;
+
+public class ArraysBidimensionales {
+
+	public static void main(String[] args) {
+		int filas = 2;
+		int columnas = 6;
+		int [][] numeros = new int [filas][columnas];
+		
+		for (int fila=0; fila<numeros.length ; fila++) {
+			
+			for (int columna=0 ; columna < numeros[fila].length   ; columna++) {
+				System.out.println("["+fila+"]["+columna+"]: "+ numeros[fila][columna]);
+			}
+			
+		}
+
+	}
+
+}
