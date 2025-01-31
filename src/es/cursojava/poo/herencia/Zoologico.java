4ec1a1b6dbdc99e7062fe34d4a4f0c1611f8b542
jlllorente3 <joseluis.llorenteperales+3@gmail.com>
2025-01-31 18:00:06 +0100

    Ejecicio Herencia Banda Música

--- src/es/cursojava/poo/herencia/Zoologico.java
+++ src/es/cursojava/poo/herencia/Zoologico.java
@@ -21,6 +21,8 @@ public class Zoologico {
         vaca1.comer();
         //vaca1.mugir();//No se puede la clase Animal no tiene el mugir
 
+        Animal leon1 = new Leon(true,"Asdas",9);
+
         Vaca vaca2 = (Vaca)vaca1;
         vaca2.mugir();
     }
